35dd4b135f2891e5a26ea7ccedaa3a015bd8f80e
dmbharath <dmbharath2002@gmail.com>
2024-08-20 11:35:53 +0530

    Added mappings

--- src/main/java/com/example/shovo_project1/controller/ProfileController.java
+++ src/main/java/com/example/shovo_project1/controller/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,15 @@ public class ProfileController
         Profile profile = profileRepository.findById(id)
         .orElseThrow(() -> new ResourceNotFoundException("Profile not found with the id : "+id));
         return ResponseEntity.ok(profile);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Profile> updateProfile(@PathVariable Long id, @RequestBody Profile profileDetails) throws ResourceNotFoundException
+    {
+        Profile profile = profileRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Profile not found with the id : "+id));
+        
+        Profile updateProfile = profileRepository.save(profile);
+        return ResponseEntity.ok(updateProfile);   
     }
 }
