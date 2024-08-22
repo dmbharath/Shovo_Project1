cb462014b8dbb188e0d353340ca43b56abf244c5
dmbharath <dmbharath2002@gmail.com>
2024-08-22 18:56:11 +0530

    Custom Query controllere file updated

--- src/main/java/com/example/shovo_project1/controller/CustomQueryController.java
+++ src/main/java/com/example/shovo_project1/controller/CustomQueryController.java
@@ -12,11 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-
+@RequestMapping("/api/customQueries")
 public class CustomQueryController
 {
     @Autowired
