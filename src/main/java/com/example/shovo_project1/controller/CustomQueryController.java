8e0b4a13e1e5d0631522f095a6e00d85de032057
dmbharath <dmbharath2002@gmail.com>
2024-08-22 09:39:34 +0530

    CustomQueryController file updated

--- src/main/java/com/example/shovo_project1/controller/CustomQueryController.java
+++ src/main/java/com/example/shovo_project1/controller/CustomQueryController.java
@@ -2,7 +2,7 @@ package com.example.shovo_project1.controller;
 
 import com.example.shovo_project1.exception.ResourceNotFoundException;
 import com.example.shovo_project1.model.CustomQuery;
-import com.example.shovo_project1.repository.CustomQueryRepository;
+import com.example.shovo_project1.service.CustomQueryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,46 +20,40 @@ import java.util.List;
 public class CustomQueryController
 {
     @Autowired
-    private CustomQueryRepository customQueryRepository;
+    private CustomQueryService customQueryService;
 
     @GetMapping
     public List<CustomQuery> getAllCustomQueries()
     {
-        return customQueryRepository.findAll();
+        return customQueryService.getAllCustomQueries();
     }
 
     @PostMapping
-    public CustomQuery createCustomQuery(@RequestBody CustomQuery customQuery)
+    public CustomQuery createCustomQuery(@RequestBody CustomQuery customQuery) throws ResourceNotFoundException
     {
-        return customQueryRepository.save(customQuery);
+        return customQueryService.createCustomQuery(customQuery);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CustomQuery> getCustomQueryById(@PathVariable Long id) throws ResourceNotFoundException
     {
-        CustomQuery customQuery = customQueryRepository.findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("Custom Query not found with the id : "+id));
+        CustomQuery customQuery = customQueryService.getCustomQueryById(id);
         return ResponseEntity.ok(customQuery);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<CustomQuery> updateCustomQuery(@PathVariable Long id, @RequestBody CustomQuery customQueryDetails) throws ResourceNotFoundException
     {
-        CustomQuery customQuery = customQueryRepository.findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("CustomQuery not found with the id : "+id));
-
-        customQuery.setQueryText((customQueryDetails.getQueryText()));
-
-        CustomQuery updatecustomQuery = customQueryRepository.save(customQuery);
-        return ResponseEntity.ok(updatecustomQuery);
+        CustomQuery updateCustomQuery = customQueryService.updateCustomQuery(id, customQueryDetails);
+        
+        updateCustomQuery.setQueryText((customQueryDetails.getQueryText()));
+        return ResponseEntity.ok(updateCustomQuery);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCustomQuery(@PathVariable Long id) throws ResourceNotFoundException
     {
-        CustomQuery customQuery = customQueryRepository.findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("Custom Query not found with the id : "+id));
-        customQueryRepository.delete(customQuery);
+        customQueryService.deleteCustomQuery(id);
         return ResponseEntity.noContent().build();
     }
 }
